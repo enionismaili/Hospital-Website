13681d84ac9e7380e7ef62507ce833d006dcf531
T-h07 <139374183+T-h07@users.noreply.github.com>
2024-03-31 15:18:12 +0200

    Create ContactUs.js

---
+++ ContactUs.js
@@ -0,0 +1,8 @@
+
+var appointmentBtn = document.getElementById("appointmentBtn");
+
+
+appointmentBtn.addEventListener("click", function() {
+    alert("Appointment has been set successfully");
+    location.reload(); // Reload the page
+});
